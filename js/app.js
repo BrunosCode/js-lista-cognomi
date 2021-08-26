47b311962e12401ac1458098967e0c9321fef760
brunEdo <edobruno.m@gmail.com>
2021-08-26 16:41:58 +0200

    js steps

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// LIST OF SURNAME
+
+// 1. Create surname array
+// 2. Ask user surname (lowercase) and push in the array
+// 3. Sort array
+// 4. Print sorted array
+// 5. Search user surname in the array
+// 6. Print user position(+1)
