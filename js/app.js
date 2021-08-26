851c32f51b11cd0fbaf4fbd00432791af5b05265
brunEdo <edobruno.m@gmail.com>
2021-08-26 16:48:28 +0200

    basic js script

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 // LIST OF SURNAME
 
 // 1. Create surname array
+let surnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 // 2. Ask user surname (lowercase) and push in the array
-// 3. Sort array
-// 4. Print sorted array
-// 5. Search user surname in the array
-// 6. Print user position(+1)
+let userSurname = prompt("What's your surname?").toLowerCase();
+surnames.push(userSurname);
+// 3. Sort and print array
+surnames.sort();
+console.log(surnames);
+// 5. Search and print user position(+1) in the array
+console.log(`You are in position: ${surnames.indexOf(userSurname) + 1}`);
